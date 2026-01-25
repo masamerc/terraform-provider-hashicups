88190562219fa2cfc6f3daa7055f0c442a9507e6
Masa Fukui <masahiro.m.fukui@gmail.com>
2026-01-25 11:12:33 +0900

    chore: update main.tf

--- examples/provider-install-verification/main.tf
+++ examples/provider-install-verification/main.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     hashicups = {
-      source = "hashicorp.com/edu/hashicups" // replaced with local path via dev_overrides in .terraformrc
+      source = "hashicorp.com/edu/hashicups" # replaced with local go binary specified in dev_overrides in .terraformrc
     }
   }
 }
 
 provider "hashicups" {}
 
-data "hachicups_coffees" "example" {}
+data "hashicups_coffees" "example" {}
